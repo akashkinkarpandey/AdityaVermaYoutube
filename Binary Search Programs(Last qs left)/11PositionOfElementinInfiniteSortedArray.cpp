9e0e9c9dd98db70bd5250c54b117d6705697306c
Akash Kinkar Pandey <104554525+akashkinkarpandey@users.noreply.github.com>
2023-01-03 01:37:28 +0530

    updated 11 of binary search

--- Binary Search Programs(Last qs left)/11PositionOfElementinInfiniteSortedArray.cpp
+++ Binary Search Programs(Last qs left)/11PositionOfElementinInfiniteSortedArray.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+//search in array with huge number of elements
+//hence take low and high within and appropriate range
+//then search to reduce search space
+
 int BinarySearch(vector<int> &nums, int target, int low, int high)
 {
     int res = -1;
