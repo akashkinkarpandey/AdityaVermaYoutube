5d70a10f0f770f74c52e4a34bf6ebb0e0150ebdf
Akash Kinkar Pandey <104554525+akashkinkarpandey@users.noreply.github.com>
2023-01-13 07:37:28 +0530

    5 minimum window substring updated

--- Sliding window algorithm(last qs left)/2variableslidingwindow/5minimumwindowsubstring.cpp
+++ Sliding window algorithm(last qs left)/2variableslidingwindow/5minimumwindowsubstring.cpp
@@ -1,3 +1,14 @@
+// Given two strings S and P. Find the smallest window in the string S consisting of all the characters(including duplicates)
+// of the string P.  Return "-1" in case there is no such window present. In case there are multiple such windows of same length, 
+// return the one with the least starting index. 
+// Example 1:
+// Input:
+// S = "timetopractice"
+// P = "toc"
+// Output: 
+// toprac
+// Explanation: "toprac" is the smallest
+// substring in which "toc" can be found.
 #include <bits/stdc++.h>
 using namespace std;
 #define fo(i, m, n) for (long i = m; i < n; i++)
@@ -28,7 +39,7 @@ int main()
                 if(j-i+1<mini)
                 {
                     mini=j-i+1;
-                    ans=s.substr(i,j+1);
+                    ans=s.substr(i,mini);
                 }
                 if (mp.find(s[i]) != mp.end())
                 {
@@ -47,3 +58,36 @@ int main()
     cout << mini<<" "<<ans<<endl;
     return 0;
 }
+// https://practice.geeksforgeeks.org/problems/smallest-window-in-a-string-containing-all-the-characters-of-another-string-1587115621/1
+// for(int i = 0, j = 0;j<s.size();)
+//     {
+//         if(mp.find(s[j])!= mp.end())
+//         {
+//             mp[s[j]]--;
+//             if (mp[s[j]] == 0)
+//                 count--;
+//         }
+//         if(count == 0)
+//         {
+//             while(count == 0)
+//             {
+//                 // mini = min(mini, j - i + 1);
+//                 if(j-i+1<mini)
+//                 {
+//                     mini=j-i+1;
+//                     ans=s.substr(i,j+1);
+//                 }
+//                 if (mp.find(s[i]) != mp.end())
+//                 {
+//                     mp[s[i]]++;
+//                     if (mp[s[i]] > 0)
+//                        { 
+//                            cout<<i<<" "<<j<<endl;
+//                            count++;
+//                        }
+//                 }
+//                 i++;
+//             }
+//         }
+//         j++;
+//     }
