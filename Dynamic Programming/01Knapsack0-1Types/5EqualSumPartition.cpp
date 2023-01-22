ee077ea12b5c7854d443655e4bfaf00b36e6b67b
Akash Kinkar Pandey <104554525+akashkinkarpandey@users.noreply.github.com>
2023-01-22 20:48:30 +0530

    updated equal sum

--- Dynamic Programming/01Knapsack0-1Types/5EqualSumPartition.cpp
+++ Dynamic Programming/01Knapsack0-1Types/5EqualSumPartition.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// gfg accepted
+// https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1
 //video 8 of aditya verma
 // Enter size
 // 4
@@ -66,3 +68,52 @@ int main()
         cout << "No,partition cannot be done\n";
     return 0;
 }
+
+// User function Template for C++
+// gfg  solution
+// class Solution{
+// public:
+//     bool subset(int n, int target,int a[], vector<vector<bool>> &dp)
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= target; j++)
+//         {
+//             if (a[i - 1] <= j)
+//             {
+//                 dp[i][j] = (dp[i - 1][j] ||
+//                             dp[i - 1][j - a[i - 1]]);
+//             }
+//             else
+//             {
+//                 dp[i][j] = dp[i - 1][j];
+//             }
+//         }
+//     }
+//     return dp[n][target];
+// }
+//     int equalPartition(int n, int a[])
+//     {
+//         // code here
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += a[i];
+//     }
+//         if ((sum & 1) == 1)
+//     {
+//         return 0;
+//     }
+//     int target=sum>>1;//(sum/2)
+//     vector<vector<bool>> dp(n + 1, vector<bool>(target + 1));
+//     // if n=0,no target sum can be reached ,
+//     // hence 1st row should be zero
+//     for (int i = 0; i <= target; i++)
+//         dp[0][i] = false;
+//     // if target is zero,any given array will have the empty subset
+//     // so entire 0th column is true by over-writing dp[0][0]
+//     for (int i = 0; i <= n; i++)
+//         dp[i][0] = true;
+//     return subset(n, target, a, dp);
+//     }
+// };
