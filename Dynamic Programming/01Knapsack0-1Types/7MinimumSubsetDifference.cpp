3a30447557c96e6f39d14daa4ea7f17194a6a9c2
Akash Kinkar Pandey <104554525+akashkinkarpandey@users.noreply.github.com>
2023-01-23 09:59:36 +0530

    updated 7 minimum subset difference

--- Dynamic Programming/01Knapsack0-1Types/7MinimumSubsetDifference.cpp
+++ Dynamic Programming/01Knapsack0-1Types/7MinimumSubsetDifference.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-//video 10 of aditya verma
+// https://practice.geeksforgeeks.org/problems/minimum-sum-partition3317/1
+// gfg accpeted
+// video 10 of aditya verma
 // Enter size
 // 4
 // Enter array elements
@@ -10,8 +12,9 @@ using namespace std;
 
 // sum of {1,6,5} - sum of {11} = 1
 
-//find range -2*S1 instead of S2-S1
-//find S1 in 1 to range/2 
+//find minimum of (range-2*S1) instead of S2-S1
+// S1+S2=sum of entire array..->S2=Sum-S1
+//find S1 in 0 to range/2 in last row of dp table
 bool subset(int n, int target, int a[], vector<vector<bool>> &dp)
 {
     for (int i = 1; i <= n; i++)
@@ -65,3 +68,47 @@ int main()
     }
     return 0;
 }
+
+// gfg solution
+// class Solution{
+//   public:
+//     bool subset(int n, int target, int a[], vector<vector<bool>> &dp)
+//     {
+//         for (int i = 1; i <= n; i++)
+//         {
+//             for (int j = 1; j <= target; j++)
+//             {
+//                 if (a[i - 1] <= j)
+//                 {
+//                     dp[i][j] = (dp[i - 1][j] ||
+//                                 dp[i - 1][j - a[i - 1]]);
+//                 }
+//                 else
+//                 {
+//                     dp[i][j] = dp[i - 1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][target];
+//     }
+// 	int minDifference(int a[], int n)  
+// 	{ 
+// 	    // Your code goes here
+// 	int sum=0;
+// 	for(int i = 0; i < n; i++)
+//         sum += a[i];
+// 	int target = sum;
+//     vector<vector<bool>> dp(n + 1, vector<bool>(target + 1,false));
+//     for (int i = 0; i <= target; i++)
+//         dp[0][i] = false;
+//     for (int i = 0; i <= n; i++)
+//         dp[i][0] = true;
+//     subset(n, target, a, dp);
+//     int mini=INT_MAX;
+//     for(int i = target/2; i >= 0; i--)
+//         if(dp[n][i])
+//             mini=min(mini,sum-2*i);
+//     return mini;
+    
+// 	} 
+// };
